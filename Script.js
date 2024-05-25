b6a1a73acce4b3a932e99ec185c500af7b97658d
ZayanKamran <ziyanffking@gmail.com>
2024-05-25 10:34:22 +0500

    Weather Advice

---
+++ Script.js
@@ -0,0 +1,9 @@
+let Userinput = prompt("What is the temperature in your area?");
+
+if (Userinput > 40) {
+    alert("Stay Home");
+} else if (Userinput > 30) {
+    alert("Use Umbrella");
+} else if (Userinput < 20) {
+    alert("It's a Nice Day");
+}
